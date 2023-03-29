3cd49cd59e8240c5301378bfbd0c5b927f080251
Harshit <101452174+Harsh502s@users.noreply.github.com>
2023-03-29 22:35:37 +0530

    Commit

--- Python Presentation/app.py
+++ Python Presentation/app.py
@@ -13,9 +13,21 @@ with open(
     model = pickle.load(f)
     pass
 
+# Set run on save to true and wide mode to true
+
+st.set_page_config(layout="wide")
+
+# Adding a background image in web app
+
 # Title of the Web App
 
 st.title("Ad Click Prediction Web App")
+
+# Making a sidebar
+
+st.sidebar.header(
+    body="Ad Click Prediction Web App :sunglasses:",
+)
 
 # Taking Input fron the user
 
@@ -41,10 +53,25 @@ else:
     pass
 
 if st.button("Predict"):
-    ypred = model.Predict(
+    ypred = model.predict(
         [[daily_time_spent_on_site, age, area_income, daily_internet_usage, sex]]
     )
     if ypred == 0:
         st.write("The user will not click on the ad")
     else:
         st.write("The user will click on the ad")
+
+# Adding a footer at the bootom of the sidebar
+
+st.sidebar.write("Made by Harshit Singh :heart: ")
+
+page_bg_img = """
+<style>
+stApp {
+background-image: url("https://cdn.analyticsvidhya.com/wp-content/uploads/2021/06/39595st.jpeg");
+background-size: cover;
+}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
